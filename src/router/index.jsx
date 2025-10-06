ec698edbbff820dcd44f40c98f4f95427c5a66dd
vincent-guizot <vincentguizot128@gmail.com>
2025-10-06 22:37:02 +0700

    members

---
+++ src/router/index.jsx
@@ -0,0 +1,42 @@
+// router.jsx
+import { createBrowserRouter } from "react-router-dom";
+import Login from "../pages/Login";
+import Home from "../pages/Home";
+import Matches from "../pages/Matches";
+import MemberLayout from "../layouts/MemberLayout";
+import Members from "../pages/Admin/Members";
+import AddMember from "../pages/Admin/AddMember";
+import EditMember from "../pages/Admin/EditMember";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    path: "/matches",
+    element: <Matches />,
+  },
+  {
+    path: "/members",
+    element: <MemberLayout />,
+    children: [
+      {
+        path: "",
+        element: <Members />,
+      },
+      {
+        path: "/add",
+        element: <AddMember />,
+      },
+      {
+        path: "/edit/:id",
+        element: <EditMember />,
+      },
+    ],
+  },
+]);
